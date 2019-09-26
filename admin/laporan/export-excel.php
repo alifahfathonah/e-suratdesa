a3b4851882fda2e06c5066b9ae6269e2ef869006
adiidx <anonyzombies@gmail.com>
2019-09-26 16:13:20 +0700

    initial commit

--- admin/laporan/export-excel.php
+++ admin/laporan/export-excel.php
@@ -32,7 +32,7 @@
 	header("Content-Disposition: attachment; filename=Laporan Surat Ds. Kedawong.xls");
 	?>
 	<center>
-		<h2>Data Penduduk <br/> Desa Kedawong</h2>
+		<h2>Laporan <br> Surat Administrasi Desa <br> Desa Kedawong</h2>
 	</center>
 	<table border="1">
           <thead>
@@ -49,7 +49,10 @@
               include ('../../config/koneksi.php');
 
               $no = 1;
-              $qTampil = mysqli_query($connect, "SELECT * FROM surat_keterangan WHERE status_surat='selesai' UNION SELECT * FROM surat_keterangan_berkelakuan_baik WHERE status_surat='selesai' UNION SELECT * FROM surat_keterangan_domisili WHERE status_surat='selesai' UNION SELECT * FROM surat_keterangan_usaha WHERE status_surat='selesai'");
+              $qTampil = mysqli_query($connect, "SELECT penduduk.*, surat_keterangan.* FROM penduduk LEFT JOIN surat_keterangan ON surat_keterangan.nik = penduduk.nik WHERE surat_keterangan.status_surat='selesai' 
+                UNION SELECT penduduk.*, surat_keterangan_berkelakuan_baik.* FROM penduduk LEFT JOIN surat_keterangan_berkelakuan_baik ON surat_keterangan_berkelakuan_baik.nik = penduduk.nik WHERE surat_keterangan_berkelakuan_baik.status_surat='selesai' 
+                UNION SELECT penduduk.*, surat_keterangan_domisili.* FROM penduduk LEFT JOIN surat_keterangan_domisili ON surat_keterangan_domisili.nik = penduduk.nik WHERE surat_keterangan_domisili.status_surat='selesai' 
+                UNION SELECT penduduk.*, surat_keterangan_usaha.* FROM penduduk LEFT JOIN surat_keterangan_usaha ON surat_keterangan_usaha.nik = penduduk.nik WHERE surat_keterangan_usaha.status_surat='selesai'");
               foreach($qTampil as $row){
             ?>
             <tr>
@@ -76,7 +79,7 @@
               <td><?php echo $tanggal . " " . $bulanIndo[$bulan] . " " . $tahun; ?></td>
               <td><?php echo $row['nama']; ?></td>
               <td><?php echo $row['jenis_surat']; ?></td>
-              <td><?php echo $row['alamat']; ?></td>
+              <td><?php echo 'Dusun ' . $row['dusun'] . ', RT' . $row['rt'] . '/RW' . $row['rw']; ?></td>
             </tr>
             <?php
               }
